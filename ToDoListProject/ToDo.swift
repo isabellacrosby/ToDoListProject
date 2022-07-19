c72ea95c643d778937b8e3805fee133c6acda78b
IsabellaCrosby <chichimarie05@gmail.com>
2022-07-19 11:48:56 -0700

    Initial Commit

---
+++ ToDoListProject/ToDo.swift
@@ -0,0 +1,16 @@
+//
+//  ToDo.swift
+//  ToDoListProject
+//
+//  Created by scholar on 7/19/22.
+//
+
+import UIKit
+
+class ToDo {
+    
+    var name = ""
+    
+    var important = false
+    
+}
